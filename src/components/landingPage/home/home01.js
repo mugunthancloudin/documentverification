ec3237766ac2a9687a96aac38538dcd81f5f8235
mugunthancloudin <mugunthan@cloudinlabs.com>
2023-11-09 10:49:52 +0530

    responsive fixed

--- src/components/landingPage/home/home01.js
+++ src/components/landingPage/home/home01.js
@@ -8,18 +8,15 @@ export default function Home01() {
         <div className="container">
           <div className="row">
             <div className="col-lg-6">
-
-            <h1 className="homeH1">"<spam className="text-danger">Trust</spam>, but verify."
-</h1>
+              <h1 className="homeH1">
+                "<spam className="text-danger">Trust</spam>, but verify."
+              </h1>
               <div className="row text-light homeComponent ">
-              
-
-              <div className="homeBox">&nbsp;</div>
-                <h1 className="ms-5 mt-5 fw-bold">Document Verification</h1>
-                <h2 className="ms-5 mt-2 fw-bold mb-5">
+                <div className="homeBox">&nbsp;</div>
+                <h1 className="ps-5 mt-5 fw-bold">Document Verification</h1>
+                <h2 className="ps-5 mt-2 fw-bold mb-5">
                   Confirm The Documents <br></br> Supplied By Your Candidates
                 </h2>
-                
               </div>
             </div>
             <div className="col-lg-6"></div>
