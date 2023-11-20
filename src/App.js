6b4903793a6ce0c276cb3564e20c8a03161a64be
Thileepan-I <thileepan@cloudinlabs.com>
2023-11-20 12:24:21 +0530

    smart contract update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import LandingPage from "./components/pages/landingPage";
-import VerificationHome from "./components/verification/verificationHome";
-import Footer from "./components/header&footer/footer";
-import VerificationSubmit from "./components/verification/verificationSubmit";
-import { BrowserRouter, Routes,Route } from "react-router-dom";
-import VerificationHomePage from "./components/pages/verificationHomePage";
-import VerificationSubmitPage from "./components/pages/verificationSubmitPage";
+import LandingPage from "./pages/landingPage";
+// import VerificationHome from "./components/verification/verificationHome";
+// import Footer from "./components/header&footer/footer";
+// import VerificationSubmit from "./components/verification/verificationSubmit";
+// import { BrowserRouter, Routes,Route } from "react-router-dom";
+// import VerificationHomePage from "./components/pages/verificationHomePage";
+// import VerificationSubmitPage from "./components/pages/verificationSubmitPage";
 import UploadToIpfs from "./uploadFile";
 
 function App() {
@@ -22,7 +22,7 @@ function App() {
           />
         </Routes>
       </BrowserRouter> */}
-      <w3m-button/>
+      <w3m-button/> 
       <UploadToIpfs/>
       <LandingPage/>
       
